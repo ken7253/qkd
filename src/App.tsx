6b8035dc6d059e058b13d9cbf842fb504ab83a61
ken7253 <ken7253@gmail.com>
2022-06-29 22:13:55 +0900

    環境設定

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ const App: React.FC = () => {
   return (
     <>
       <p>test</p>
+      <style jsx global>{`
+        body {
+          width: 350px;
+          height: 550px;
+        }
+      `}</style>
     </>
   );
 }
