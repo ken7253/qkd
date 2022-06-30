939bd76abfed5d53bd3143e9af53bfe9536726ac
ken7253 <ken7253@gmail.com>
2022-07-01 01:03:42 +0900

    スタイル定義

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,10 @@ import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
-    <div>
+    <>
       <Contents />
       <Footer />
-      <style jsx global>{`
-        body {
-          width: 350px;
-          height: 550px;
-        }
-      `}</style>
-    </div>
+    </>
   );
 }
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,33 @@ import React from "react";
 const Footer:React.FC = () => {
   return (
     <footer>
+      <ul>
+        <li><a href="https://github.com/ken7253/qkd" target="_blank">GitHub</a></li>
+        <li><a href="https://github.com/ken7253/qkd/issues" target="_blank">Issue</a></li>
+      </ul>
       <small>© ken7253</small>
+      <style jsx>{`
+        footer {
+          display: flex;
+          justify-content: space-between;
+          align-items: flex-end;
+          padding-left: var(--l-safe-padding);
+          padding-right: var(--l-safe-padding);
+          padding-bottom: 5px;
+          background-color: var(--c-darker-static);
+          color: var(--c-lighter-static);
+        }
+        ul {
+          display: flex;
+          gap: 8px;
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+        }
+        a {
+          color: inherit;
+        }
+      `}</style>
     </footer>
   );
 }
