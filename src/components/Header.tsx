fc918f8d679e49812fafcfb129df03bb4962db09
ken7253 <ken7253@gmail.com>
2022-07-02 16:01:30 +0900

    マニュフェストファイルとタイトルなどが連動するように修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 
 const Header:React.FC = () => {
+  const {version, name} = chrome.runtime.getManifest();
+
   return(
     <header>
       <img src="/img/icon.svg" alt="" height="48" width="48" />
-      <h1>Quick Docs</h1>
+      <h1>{name}</h1>
+      <small>{version}</small>
       <style jsx>{`
         header {
           display: flex;
           align-items: center;
-          width: 100%;
-          padding-left: var(--l-safe-padding);
-          padding-right: var(--l-safe-padding);
+          max-width: 100%;
+          padding: 5px var(--l-safe-padding);
           background-color: var(--c-theme-main);
           color: var(--c-theme-light);
         }
@@ -20,8 +22,14 @@ const Header:React.FC = () => {
           width: auto;
         }
         h1 {
+          flex: 1 1 auto;
           padding-left: 8px;
           font-weight: 400;
+          margin: 0;
+        }
+        small {
+          align-self: flex-end;
+          letter-spacing: .1em;
         }
       `}</style>
     </header>
