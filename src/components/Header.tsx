81285fceff1b3cfdf8a7c576e724e73e770f02bf
ken7253 <ken7253@gmail.com>
2022-07-02 15:02:15 +0900

    ヘッダにアイコンを表示

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import React from "react";
 const Header:React.FC = () => {
   return(
     <header>
+      <img src="/img/icon.svg" alt="" height="48" width="48" />
       <h1>Quick Docs</h1>
       <style jsx>{`
         header {
@@ -13,6 +14,14 @@ const Header:React.FC = () => {
           padding-right: var(--l-safe-padding);
           background-color: var(--c-theme-main);
           color: var(--c-theme-light);
+        }
+        img {
+          max-height: 100%;
+          width: auto;
+        }
+        h1 {
+          padding-left: 8px;
+          font-weight: 400;
         }
       `}</style>
     </header>
